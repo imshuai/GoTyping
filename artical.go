11f23927abd5807652e0bae7f3e71793ec22ac98
irislee <iris-me@live.com>
2017-05-16 17:13:03 +0800

    Signed-off-by: irislee <iris-me@live.com>

--- artical.go
+++ artical.go
@@ -1,23 +1,57 @@
 package main
 
 import (
+	"errors"
 	"time"
 )
 
 type Artical struct {
-	ID         int64
-	Name       string    `xorm:"varchar(255) index"`
-	Summary    string    `xorm:"varchar(255) index"`
-	Content    string    `xorm:"text"`
-	Author     string    `xorm:"varchar(30)"`
-	CreateTime time.Time `xorm:"created"`
-	UpdateTime time.Time `xorm:"updated"`
+	ID         int64     `json:"id"`
+	Name       string    `xorm:"varchar(255) index" json:"name"`
+	Alias      string    `xorm:"varchar(255) index" json:"alias"`
+	Summary    string    `xorm:"varchar(255) index" json:"summary"`
+	Content    string    `xorm:"text" json:"content"`
+	Author     string    `xorm:"varchar(30)" json:"author"`
+	CreateTime time.Time `xorm:"created" json:"create_time"`
+	UpdateTime time.Time `xorm:"updated" json:"update_time"`
+}
+
+type ArticalSummary struct {
 }
 
 func NewArtical() *Artical {
 	return &Artical{}
 }
 
-func (atl *Artical) Insert() *Artical {
+func GetArtical(ID int64) *Artical {
+	if ID < 0 {
+		return &Artical{}
+	} else if ID == 0 {
+		return NewArtical()
+	}
+	atl := new(Artical)
+	exist, err := db.ID(ID).Get(atl)
+	if err != nil || !exist {
+		return &Artical{}
+	}
+	return atl
+}
 
+func (atl *Artical) Insert() (bool, error) {
+	if atl.ID != 0 {
+		return false, errors.New("artical already been inserted")
+	}
+	effect, err := db.InsertOne(atl)
+	if err != nil || effect != 1 {
+		return false, err
+	}
+	return true, nil
+}
+
+func (atl *Artical) Update() (bool, error) {
+	return true, nil
+}
+
+func (atl *Artical) Delete() (bool, error) {
+	return true, nil
 }
--- comment.go
+++ comment.go
@@ -5,16 +5,48 @@ import (
 )
 
 type Comment struct {
-	ID         int64
+	ID         int64 `json:"id"`
 	ArticalID  int64
-	Author     string    `xorm:"varchar(30)"`
-	Content    string    `xorm:"text"`
-	CreateTime time.Time `xorm:"created"`
-	CommentTo  int64     `xorm:"default(0)"`
-	Like       int64     `xorm:"default(0)"`
-	Unlike     int64     `xorm:"default(0)"`
+	Author     string    `xorm:"varchar(30)" json:"author"`
+	Content    string    `xorm:"text" json:"content"`
+	CreateTime time.Time `xorm:"created" json:"time"`
+	CommentTo  int64     `xorm:"default(0)" json:"comment_to"`
+	Like       int64     `xorm:"default(0)" json:"like"`
+	Unlike     int64     `xorm:"default(0)" json:"unlike"`
+}
+
+type ArticalComments struct {
+	ArticalID int64     `json:"artical_id"`
+	Comments  []Comment `json:"comments"`
 }
 
 func NewComment() *Comment {
 	return &Comment{}
+}
+
+func (cm *Comment) Insert() (bool, error) {
+	if cm.ID != 0 {
+		return false, errors.New("comment already been inserted")
+	}
+	effect, err := db.InsertOne(cm)
+	if err != nil || effect != 1 {
+		return false, err
+	}
+	return true, nil
+}
+
+func (cm *Comment) Delete() (bool, error) {
+	return true, nil
+}
+
+func GetComments(articalID int64) (ArticalComments, error) {
+	cms := make([]Comment, 0)
+	err := db.Where("`artical_id`=?", articalID).Find(&cms)
+	if err != nil {
+		return ArticalComments{}, err
+	}
+	return ArticalComments{
+		ArticalID: articalID,
+		Comments:  cms,
+	}, nil
 }
